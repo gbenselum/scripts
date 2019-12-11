e6eaf9a1ed5f0bc91b364f209488e4d3fb582d59
Gabriel Benselum <49731558+gbenselum@users.noreply.github.com>
2019-12-11 11:43:06 -0300

    Create hostsfile.sh

---
+++ hostsfile.sh
@@ -0,0 +1,3 @@
+# el comando lista las IPs entregadas por KVM y las parsea para entregar al archivo hosts
+
+sudo virsh net-dhcp-leases default | grep ipv4 | awk '{print substr($5, 1, length($5)-3) "\t" $6}' >> /etc/hosts
