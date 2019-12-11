581188509519ef93c403cd51c00d4ebb30c59537
Gabriel Benselum <49731558+gbenselum@users.noreply.github.com>
2019-12-11 14:22:31 -0300

    Update hostsfile.sh

--- hostsfile.sh
+++ hostsfile.sh
@@ -1,3 +1,3 @@
-# lista las IPs entregadas por KVM y las parsea para entregar al archivo hosts requiere la existencia del archivo y correr como root
+# lista las IPs entregadas por KVM y las parsea para entregar al archivo hosts con los nombres de las VMs y las IPs correspondientes. requiere la existencia del archivo y correr como root
 
 virsh net-dhcp-leases default | grep ipv4 | awk '{print substr($5, 1, length($5)-3) "\t" $6}' >> /etc/hosts
