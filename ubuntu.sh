e11adfd32c03613ea186620ac013bb5ddeb3430a
Gabriel Benselum <49731558+gbenselum@users.noreply.github.com>
2020-06-24 19:41:16 -0300

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,12 @@
+sudo apt purge snapd
+
+sudo apt update
+sudo apt install vlc ansible arc-theme
+
+#vscode
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
