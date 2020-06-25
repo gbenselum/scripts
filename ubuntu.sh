ac919a9fc341cdfe854c4468c8f9d89630b3bd6e
Gabriel Benselum <49731558+gbenselum@users.noreply.github.com>
2020-06-25 18:17:35 -0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -23,6 +23,6 @@ sudo apt update
 
 
 
-sudo apt install virtualbox vim tmux gnome-shell-extensions ubuntustudio-wallpapers git vlc ansible arc-theme gnome-tweak-tool steam code lutris docker-ce docker-ce-cli containerd.io -y
+sudo apt install vagrant virtualbox vim tmux gnome-shell-extensions ubuntustudio-wallpapers git vlc ansible arc-theme gnome-tweak-tool steam code lutris docker-ce docker-ce-cli containerd.io -y
 
 
