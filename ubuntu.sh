7f5bdb57a4db859a1289b1d7d7d718f2afb64592
Gabriel Benselum <49731558+gbenselum@users.noreply.github.com>
2020-06-26 23:33:35 -0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,6 +8,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https  ca-certificates curl gnupg-agent software-properties-common -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io    
 sudo add-apt-repository   "deb [arch=amd64] https://download.docker.com/linux/ubuntu    $(lsb_release -cs)    stable"
+sudo apt install docker-ce docker-ce-cli containerd.io -y
 
 #vscode
 sudo apt update
@@ -18,20 +19,24 @@ sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/v
 # repo section
 sudo add-apt-repository multiverse
 sudo add-apt-repository universe
-sudo add-apt-repository ppa:lutris-team/lutris
 sudo apt update
 
 
 # ubuntu
-sudo apt install  vim tmux gnome-shell-extensions ubuntustudio-wallpapers  vlc ansible arc-theme gnome-tweak-tool steam  lutris docker-ce docker-ce-cli containerd.io -y
-vagrant virtualbox
+sudo apt install  gnome-shell-extensions ubuntustudio-wallpapers  vlc ansible   
 
-# pop
-# sudo apt install  vim tmux gnome-shell-extensions git vlc  gnome-tweak-tool steam code lutris  -y
+# sudo apt install  vim tmux  git vlc  gnome-tweak-tool steam code lutris  -y
 
+# gnome utils
+sudo apt install gnome-shell-extensions arc-theme gnome-tweak-tool -y
+
+# gaming and extras
+sudo add-apt-repository ppa:lutris-team/lutris
+sudo apt install steam  lutris vlc -y
 
 # Vms
-sudo apt install vagrant virtualbox
+sudo apt install vagrant virtualbox -y
+
 # DEV ansible, Python and more
-sudo apt install python3-pip git code 
+sudo apt install python3-pip git code tmux -y
 sudo pip3 install ansible-lint
